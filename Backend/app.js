97a65bd6151f59d4e6c51e82ac48473039d7e6cd
Vasudha <89298030+VasudhaShivane@users.noreply.github.com>
2025-03-26 12:32:38 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -52,7 +52,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.st4q7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
     console.log(err);
